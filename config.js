97e20bd3004c91d1e8f9dbff643bc3c1f92c462b
Tomas Bartkus <to.bartkus@gmail.com>
2014-06-07 12:10:16 +0300

    Updated configuration

--- config.js
+++ config.js
@@ -1,38 +1,39 @@
 if (Meteor.isClient) {
-    CmsInn.configPlugins({
-        navigation: {
-            pageTypes: [
-                {
-                    type: "about"
-                },
-                {
-                    type: "services"
-                },
-                {
-                    type: "one_column_portfolio"
-                },
-                {
-                    type: "single_portfolio_item"
-                },
-                {
-                    type: "blog_home_first"
-                },
-                {
-                    type: "blog_home_second"
-                },
-                {
-                    type: "blog_post_item"
-                },
-                {
-                    type: "home"
-                }
-            ]
-        }
-    });
-
     // Will proxy this config to Router.configure( ... );
     CmsInn.configure({
         loadingTemplate: 'loading',
-        layoutTemplate: 'layout'
+        layoutTemplate: 'layout',
+        plugins: {
+            navigation: {
+                pageTypes: [
+                    {
+                        type: "about"
+                    },
+                    {
+                        type: "services"
+                    },
+                    {
+                        type: "one_column_portfolio"
+                    },
+                    {
+                        type: "single_portfolio_item"
+                    },
+                    {
+                        type: "blog_home_first"
+                    },
+                    {
+                        type: "blog_home_second"
+                    },
+                    {
+                        type: "blog_post_item"
+                    },
+                    {
+                        type: "home"
+                    }
+                ],
+                defaultTemplate: 'home'     // On initial load as there is no routes, 
+                                            // package automatically adds route and uses 'defaultTemplate' value to set as its template
+            }
+        }
     });
 }
